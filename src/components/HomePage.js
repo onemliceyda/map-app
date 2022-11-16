c6190f8a8b3b646b578dae9fe457001e71d16a42
Ceyda <ceydaonemli2@gmail.com>
2022-11-16 16:44:36 +0300

    markers added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,24 +1,30 @@
 import React from "react"
 import { useNavigate } from "react-router"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup,FeatureGroup } from "react-leaflet"
 import ankaraDistricts from "../data/ankaraDistricts.json"
+import { EditControl } from "react-leaflet-draw"
+import "leaflet-draw/dist/leaflet.draw.css"
 
 const HomePage = () => {
-  console.log(ankaraDistricts)
-
+const handleCreated=(e)=>console.log(e)
   return (
-    <MapContainer
-      center={[42.585444, 13.257684]}
-      zoom={13}
-      scrollWheelZoom={true}
-    >
+    <MapContainer center={[39.93, 32.85]} zoom={13} scrollWheelZoom={true}>
+      <FeatureGroup>
+        <EditControl position="topright" onCreated={handleCreated}/>
+      </FeatureGroup>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       {ankaraDistricts.map((ankara) => (
-        <Marker key={ankara.id} position={[ankara.latitude, ankara.longitude]}></Marker>
+        <Marker key={ankara.id} position={[ankara.latitude, ankara.longitude]}>
+          <Popup position={[ankara.latitude, ankara.longitude]}>
+            <div>
+              <h2>{"Bölge: " + ankara.name}</h2>
+            </div>
+          </Popup>
+        </Marker>
       ))}
     </MapContainer>
   )
